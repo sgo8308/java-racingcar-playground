fb1fcfffeb5b03b38e1a4e18516f15c9f2613858
jiwoo <sgo8308@naver.com>
2022-01-13 00:22:01 +0900

    feat(racingcar.domain) : add compareTo() and isSamePositionAs() to Car

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,6 +1,9 @@
 package racingcar.domain;
 
-public class Car {
+import java.util.Objects;
+
+public class Car implements Comparable<Car> {
+
     private CarName name;
     private CarPosition position;
 
@@ -11,5 +14,32 @@ public class Car {
 
     public void move() {
         position.addPosition();
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.position.compareTo(o.position);
+    }
+
+    public boolean isSamePositionAs(Car car) {
+        return this.position.equals(car.position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name) && Objects.equals(position,
+                car.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
     }
 }
