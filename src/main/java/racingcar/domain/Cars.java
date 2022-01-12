a4700d3d260e92e8366c97426876ec959e583c4e
jiwoo <sgo8308@naver.com>
2022-01-13 00:31:55 +0900

    feat(racingcar.domain) : create Cars and CarsTest and implement findWinCars() and equals()
    findWinCars() Test case
     - return Cars which has farthest car
     - return Cars which has farthest cars if farthest car is more than two
    
    equlas() Test case
     - has to be the same regardless of the order of Car
     - has to be the same if Cars has Car that have same name and same position.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,67 @@
+package racingcar.domain;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Cars {
+
+    List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public Cars findWinCars() {
+        Car winCar = findWinCar();
+        return findCarsAtSamePosition(winCar);
+    }
+
+    private Car findWinCar() {
+        Collections.sort(cars, Collections.reverseOrder());
+        return cars.get(0);
+    }
+
+    private Cars findCarsAtSamePosition(Car winCar) {
+        List<Car> winCars = cars.stream()
+                .filter(winCar::isSamePositionAs)
+                .collect(Collectors.toList());
+
+        return new Cars(winCars);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Cars cars1 = (Cars) o;
+
+        return hasSameCar(cars1) && isSameSize(cars1);
+    }
+
+    private boolean hasSameCar(Cars cars) {
+        return this.cars.stream().allMatch(cars::contains);
+    }
+
+    private boolean isSameSize(Cars cars) {
+        return this.size() == cars.size();
+    }
+
+    private boolean contains(Car car) {
+        return cars.contains(car);
+    }
+
+    private int size() {
+        return cars.size();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cars);
+    }
+}
---
+++ src/test/java/racingcar/domain/CarsTest.java
@@ -0,0 +1,70 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class CarsTest {
+
+    @Test
+    void findWinCars_가장_멀리_있는_자동차가_포함된_Cars를_반환해야_한다() {
+        //given
+        Car carAtPosition1 = new Car(new CarName("name1"), CarPosition.fromCustomPosition(1));
+        Car carAtPosition3 = new Car(new CarName("name3"), CarPosition.fromCustomPosition(3));
+        Car carAtPosition5 = new Car(new CarName("name5"), CarPosition.fromCustomPosition(5));
+
+        Cars cars = new Cars(Arrays.asList(carAtPosition1, carAtPosition3, carAtPosition5));
+
+        //when
+        Cars winCars = cars.findWinCars();
+
+        Cars expectedWinCars = new Cars(Arrays.asList(carAtPosition5));
+        //then
+        assertThat(winCars).isEqualTo(expectedWinCars);
+    }
+
+    @Test
+    void findWinCars_가장_멀리_있는_자동차가_2개이상일땐_그차들을_모두_포함해서_Cars를_반환() {
+        //given
+        Car carAtPosition1 = new Car(new CarName("name1"), CarPosition.fromCustomPosition(1));
+        Car carAtPosition3 = new Car(new CarName("name3"), CarPosition.fromCustomPosition(3));
+        Car carAtPosition5_1 = new Car(new CarName("n5_1"), CarPosition.fromCustomPosition(5));
+        Car carAtPosition5_2 = new Car(new CarName("n5_2"), CarPosition.fromCustomPosition(5));
+
+        Cars cars = new Cars(
+                Arrays.asList(carAtPosition1, carAtPosition3, carAtPosition5_1, carAtPosition5_2));
+
+        //when
+        Cars winCars = cars.findWinCars();
+
+        Cars expectedWinCars = new Cars(Arrays.asList(carAtPosition5_1, carAtPosition5_2));
+        Cars expectedWinCarsReverseOrder = new Cars(
+                Arrays.asList(carAtPosition5_2, carAtPosition5_1));
+        //then
+        assertThat(winCars).isEqualTo(expectedWinCars).isEqualTo(expectedWinCarsReverseOrder);
+    }
+
+    @Test
+    void equals_순서_상관_없이_동일한_차들을_포함하고_있으면_같다() {
+        //given
+        Car car1 = new Car(new CarName("name1"), CarPosition.fromStartPosition());
+        Car car2 = new Car(new CarName("name2"), CarPosition.fromStartPosition());
+
+        Cars actualCars = new Cars(Arrays.asList(car1, car2));
+
+        Car carSameAsCar1 = new Car(new CarName("name1"), CarPosition.fromStartPosition());
+        Car carSameAsCar2 = new Car(new CarName("name2"), CarPosition.fromStartPosition());
+
+        Cars expectedCars = new Cars(Arrays.asList(carSameAsCar1, carSameAsCar2));
+        Cars expectedCarsReverseOrder = new Cars(Arrays.asList(carSameAsCar2, carSameAsCar1));
+
+        //when
+        boolean isEqualWithExpectedCars = actualCars.equals(expectedCars);
+        boolean isEqualWithExpectedCarsReversOrder = actualCars.equals(expectedCarsReverseOrder);
+
+        //then
+        assertThat(isEqualWithExpectedCars).isTrue();
+        assertThat(isEqualWithExpectedCarsReversOrder).isTrue();
+    }
+}
