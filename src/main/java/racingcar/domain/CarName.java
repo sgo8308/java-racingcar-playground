b3a89cb722a95ecbba9b3977f7c6b5c8ed885e33
jiwoo <sgo8308@naver.com>
2022-01-12 18:26:24 +0900

    feat(racingcar.domain) : add validation() to CarName
    To validate if name is less than 6

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,9 +1,35 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class CarName {
-    String name;
+    private String name;
 
     public CarName(String name) {
+        validation(name);
         this.name = name;
+    }
+
+    private void validation(String name) throws IllegalArgumentException {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없습니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
