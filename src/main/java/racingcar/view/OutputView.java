97fdc069cd2cc8eebecc9c31aef094beb13f79c5
jiwoo <sgo8308@naver.com>
2022-01-13 22:23:48 +0900

    feat(racingcar.domain) : Add methods to OutputView and change all methods to static
    Add printMessageToGetCarNames(), printMessageToGetRound(), printBlank() to OutputView

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -2,15 +2,26 @@ package racingcar.view;
 
 public class OutputView {
 
-    public void printCarNameAndPosition(String name, int position) {
-        StringBuilder positionString = new StringBuilder();
-        for (int i = 0; i < position; i++) {
-            positionString.append("-");
-        }
-        System.out.println(name + " : " + positionString);
+    private OutputView() {
     }
 
-    public void printWinCars(String winners) {
+    public static void printMessageToGetCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    }
+
+    public static void printMessageToGetRound() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printCarNameAndPosition(String name, String position) {
+        System.out.println(name + " : " + position);
+    }
+
+    public static void printWinCars(String winners) {
         System.out.println(winners + "가 최종 우승했습니다.");
+    }
+
+    public static void printBlank() {
+        System.out.println();
     }
 }
