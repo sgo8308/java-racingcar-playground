ca202a0be56736d59534970f9554e0c46d81a678
jiwoo <sgo8308@naver.com>
2022-01-16 00:28:13 +0900

    feat(racingcar.domain) : add varible winnerNames to WinnersDTOand add getter,setter

--- src/main/java/racingcar/domain/WinnersDTO.java
+++ src/main/java/racingcar/domain/WinnersDTO.java
@@ -1,5 +1,16 @@
 package racingcar.domain;
 
+import java.util.List;
+
 public class WinnersDTO {
 
+    List<String> winnersNames;
+
+    public List<String> getWinnersNames() {
+        return null;
+    }
+
+    public void setWinnersNames(List<String> winnersNames) {
+        this.winnersNames = winnersNames;
+    }
 }
