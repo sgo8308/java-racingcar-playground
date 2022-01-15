11b2cd317345a661ff0bf0d6b50672c093879bcf
jiwoo <sgo8308@naver.com>
2022-01-16 00:27:09 +0900

    feat(racingcar.domain) : create CarDTO and add getter,setter

---
+++ src/main/java/racingcar/domain/CarDTO.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+public class CarDTO {
+
+    String name;
+    int position;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
