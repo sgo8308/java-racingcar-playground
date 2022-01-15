dbfdcf2ec8207ac193cf8aace1d4580023ffeb03
jiwoo <sgo8308@naver.com>
2022-01-16 00:32:15 +0900

    feat(racingcar.view) : implement methods of OutputView
    Implement method
     - printCarNamesAndPosition
     - printWinners

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,16 +1,43 @@
 package racingcar.view;
 
-import racingcar.domain.CarsDTO;
+import java.util.List;
+import racingcar.domain.CarDTO;
 import racingcar.domain.WinnersDTO;
 
 public class OutputView {
 
-    public static void printCarNamesAndPosition(CarsDTO carsDTO) {
+    public static void printCarNamesAndPosition(List<CarDTO> carDTOs) {
+        String message = "";
+        for (int i = 0; i < carDTOs.size(); i++) {
+            CarDTO carDTO = carDTOs.get(i);
+            message = carDTO.getName() + " : " + getPositionString(carDTO);
+        }
+        System.out.println(message);
+    }
 
+    private static String getPositionString(CarDTO carDTO) {
+        String position = "";
+        for (int j = 0; j < carDTO.getPosition(); j++) {
+            position += "-";
+        }
+        return position;
     }
 
     public static void printWinners(WinnersDTO winnersDTO) {
-
+        List<String> winners = winnersDTO.getWinnersNames();
+        StringBuilder winnerNames = new StringBuilder();
+        buildWinnerString(winners, winnerNames);
+        System.out.println(winnerNames);
     }
 
+    private static void buildWinnerString(List<String> winners, StringBuilder winnerNames) {
+        for (int i = 0; i < winners.size(); i++) {
+            winnerNames.append(winners.get(i));
+
+            if (i == winners.size() - 1) {
+                continue;
+            }
+            winnerNames.append(", ");
+        }
+    }
 }
