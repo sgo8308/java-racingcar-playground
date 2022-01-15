29f7976988413c6c855eee3b9300a7a2254e8fc1
jiwoo <sgo8308@naver.com>
2022-01-16 01:19:15 +0900

    feat(racingcar.domain) : create CarName and add equals() and hashCode()

---
+++ src/main/java/racingcar/domain/CarName.java
@@ -0,0 +1,29 @@
+package racingcar.domain;
+
+import java.util.Objects;
+
+public class CarName {
+
+    String name;
+
+    public CarName(String s) {
+        this.name = s;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
