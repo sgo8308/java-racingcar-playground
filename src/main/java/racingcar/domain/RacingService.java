fc47792f50c40396b4abc666ea7f9ca66e68f64d
jiwoo <sgo8308@naver.com>
2022-01-17 00:21:41 +0900

    feat(racingcar.domain) : add isEnd() to RacingService and implement readyCars(), setRound()

--- src/main/java/racingcar/domain/RacingService.java
+++ src/main/java/racingcar/domain/RacingService.java
@@ -4,12 +4,26 @@ import java.util.List;
 
 public class RacingService {
 
+    Cars cars;
+    Round round;
+
     public RacingService(String carNames, int totalRound) {
+        readyCars(carNames);
+        setRound(totalRound);
+    }
+
+    private void readyCars(String carNames) {
+        List<CarName> carNameList = CarNameParser.parseCarNames(carNames);
+        cars = CarService.createCars(carNameList);
 
     }
 
-    public boolean isEnd() {
-        return false;
+    private void setRound(int totalRound) {
+        round = new Round(totalRound);
+    }
+
+    public boolean isEnd(int round) {
+        return this.round.isEnd(round);
     }
 
     public void race() {
