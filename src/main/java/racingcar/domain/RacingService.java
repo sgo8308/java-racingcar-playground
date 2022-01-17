7419e145027f521335419e6da043aa107dc391f3
jiwoo <sgo8308@naver.com>
2022-01-17 14:40:34 +0900

    feat(racingcar.domain) : implement race() in RacingService and modify constructor to set MovingRule

--- src/main/java/racingcar/domain/RacingService.java
+++ src/main/java/racingcar/domain/RacingService.java
@@ -6,16 +6,18 @@ public class RacingService {
 
     Cars cars;
     Round round;
+    MovingRule rule;
 
-    public RacingService(String carNames, int totalRound) {
+    public RacingService(String carNames, int totalRound,
+            RandomMovingRule randomMovingRule) {
         readyCars(carNames);
         setRound(totalRound);
+        rule = randomMovingRule;
     }
 
     private void readyCars(String carNames) {
         List<CarName> carNameList = CarNameParser.parseCarNames(carNames);
         cars = CarService.createCars(carNameList);
-
     }
 
     private void setRound(int totalRound) {
@@ -27,7 +29,7 @@ public class RacingService {
     }
 
     public void race() {
-
+        cars.move(rule);
     }
 
     public List<CarDTO> getCarsDTO() {
