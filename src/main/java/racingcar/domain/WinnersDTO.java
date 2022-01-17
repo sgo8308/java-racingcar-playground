644eae9b69b101b72ef8ca251aa815b6264c6d14
jiwoo <sgo8308@naver.com>
2022-01-17 17:38:40 +0900

    feat(racingcar.domain) : add constructor, hashcode(), equlas() to WinnersDTO and remove setter

--- src/main/java/racingcar/domain/WinnersDTO.java
+++ src/main/java/racingcar/domain/WinnersDTO.java
@@ -1,16 +1,34 @@
 package racingcar.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class WinnersDTO {
 
     List<String> winnersNames;
 
-    public List<String> getWinnersNames() {
-        return null;
+    public WinnersDTO(List<String> winnerNames) {
+        this.winnersNames = winnerNames;
     }
 
-    public void setWinnersNames(List<String> winnersNames) {
-        this.winnersNames = winnersNames;
+    @Override
+    public int hashCode() {
+        return Objects.hash(getWinnersNames());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        WinnersDTO that = (WinnersDTO) o;
+        return Objects.equals(getWinnersNames(), that.getWinnersNames());
+    }
+
+    public List<String> getWinnersNames() {
+        return winnersNames;
     }
 }
