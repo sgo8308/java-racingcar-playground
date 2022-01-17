205deb736b7852c8ab359fe766948606caf83a9e
jiwoo <sgo8308@naver.com>
2022-01-17 14:21:11 +0900

    refactor(racingcar.domain) : modify text of createCars() in CarServiceText

--- src/test/java/racingcar/domain/CarServiceTest.java
+++ src/test/java/racingcar/domain/CarServiceTest.java
@@ -1,6 +1,6 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,22 +10,20 @@ class CarServiceTest {
     @Test
     void createCars() {
         //given
-        String carName = "jiwoo,seungho";
+        String carName = "jiwoo,soho";
         List<CarName> carNames = CarNameParser.parseCarNames(carName);
 
         //when
         Cars cars = CarService.createCars(carNames);
-
-        Car jiwoo = new Car(carNames.get(0), new CarPosition(0));
-        Car seungho = new Car(carNames.get(1), new CarPosition(0));
-        ArrayList<Car> carList = new ArrayList<>();
-        carList.add(jiwoo);
-        carList.add(seungho);
-
-        Cars expectedCars = new Cars(carList);
+        Cars expectedCars = new Cars(Arrays.asList(
+                new Car(carNames.get(0), new CarPosition(0)),
+                new Car(carNames.get(1), new CarPosition(0))
+        ));
 
         //then
         Assertions.assertThat(cars).isEqualTo(expectedCars);
     }
-
 }
+
+
+
